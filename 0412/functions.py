34d0b5ad8cff51f65c89e867d77f89b7bba7d4e8
hwookim <gusdn9754@gmail.com>
2021-04-12 10:12:29 +0900

    refactor: 함수 분리

---
+++ 0412/functions.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def seq2dataset(seq, window_size, code2idx):
+    dataset = []
+    for i in range(len(seq) - window_size):
+        subset = seq[i:(i + window_size + 1)]
+        dataset.append([code2idx[item] for item in subset])
+    return np.array(dataset)
